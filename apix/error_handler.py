9ce9e4721779606a70af0b5bd9ce222e41b85e5e
Danijel <dan@apix.org>
2023-07-19 22:46:19 +0200

    Add argument validation for ApixErrorHandler

--- apix/error_handler.py
+++ apix/error_handler.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from inspect import signature
 from typing import Callable, Type, TYPE_CHECKING
 
 
@@ -14,14 +15,39 @@ __all__ = [
 
 class ApixErrorHandler:
 
-    def __init__(
-            self,
-            error: Type[Exception],
+    def __new__(
+            cls,
+            error_type: Type[Exception],
             handle: Callable[[Exception], ApixError],
     ):
 
-        self.error = error
-        self.handle = handle
+        if not issubclass(error_type, Exception):
+            raise TypeError("The argument 'error' must be a subclass of Exception")
+
+        if not callable(handle):
+            raise TypeError("The argument 'handle' must be a function")
+        elif len(signature(handle).parameters) != 1:
+            raise TypeError("The argument 'handle' must be a function with exactly one argument")
+
+        return super().__new__(cls)
+
+    def __init__(
+            self,
+            error_type: Type[Exception],
+            handle: Callable[[Exception], ApixError],
+    ):
+
+        self.error_type = error_type
+        self._handle = handle
 
     def __repr__(self) -> str:
-        return f'<{self.__class__.__name__}:{self.error.__name__}>'
+        return f'<{self.__class__.__name__}:{self.error_type.__name__}>'
+
+    def handle(self, error: Exception) -> ApixError:
+
+        apix_error = self._handle(error)
+
+        if not isinstance(apix_error, ApixError):
+            raise TypeError(f"The 'handle' function must return an ApixError")
+
+        return apix_error
