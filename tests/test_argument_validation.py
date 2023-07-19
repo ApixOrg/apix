f3d4e5bf08a5d61db1cd63fe2bb64f7703f4d610
Danijel <dan@apix.org>
2023-07-19 22:51:41 +0200

    Add test cases for argument validation of ApixErrorHandler

--- tests/test_argument_validation.py
+++ tests/test_argument_validation.py
@@ -1,6 +1,7 @@
 import pytest
 
 from apix.error import *
+from apix.error_handler import *
 
 
 def test_valid_apix_error():
@@ -19,3 +20,22 @@ def test_invalid_message_apix_error():
 def test_invalid_code_apix_error():
     with pytest.raises(TypeError):
         ApixError('Some message', 0)
+
+
+def test_valid_apix_error_handler():
+    ApixErrorHandler(ZeroDivisionError, lambda error: ApixError('Some message'))
+
+
+def test_invalid_error_type_apix_error_handler():
+    with pytest.raises(TypeError):
+        ApixErrorHandler(ZeroDivisionError(), lambda error: ApixError('Some message'))
+
+
+def test_invalid_handle_1_apix_error_handler():
+    with pytest.raises(TypeError):
+        ApixErrorHandler(ZeroDivisionError, None)
+
+
+def test_invalid_handle_2_apix_error_handler():
+    with pytest.raises(TypeError):
+        ApixErrorHandler(ZeroDivisionError, lambda error1, error2: ApixError('Some message'))
