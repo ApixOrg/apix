e77b994a2040134b6bc919b3bb7b3ac04cfb4b30
Danijel <dan@apix.org>
2023-08-02 16:13:03 +0200

    add error handler to imports

--- apix/__init__.py
+++ apix/__init__.py
@@ -5,6 +5,7 @@ from apix.authenticator import ApixAuthenticator
 from apix.context import ApixContext
 from apix.database import ApixDatabase, ApixAsyncDatabase
 from apix.error import ApixError
+from apix.error_handler import ApixErrorHandler
 from apix.model import ApixModel
 from apix.resolver import ApixQueryResolver, ApixMutationResolver
 from apix.scalar import ApixId, ApixString, ApixInteger, ApixFloat, ApixBoolean, ApixDateTime
