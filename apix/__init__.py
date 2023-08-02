041f53f44f5daa8850ceeadca90c4e51a97fe39b
Danijel <dan@apix.org>
2023-08-02 16:11:42 +0200

    sort imports alphabetically

--- apix/__init__.py
+++ apix/__init__.py
@@ -1,7 +1,7 @@
 
 from apix.app import ApixApp
-from apix.authenticator import ApixAuthenticator
 from apix.attribute import ApixIdAttribute, ApixStringAttribute, ApixIntegerAttribute, ApixFloatAttribute, ApixBooleanAttribute, ApixDateTimeAttribute, ApixObjectAttribute, ApixReferenceAttribute
+from apix.authenticator import ApixAuthenticator
 from apix.context import ApixContext
 from apix.database import ApixDatabase, ApixAsyncDatabase
 from apix.error import ApixError
