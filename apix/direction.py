f7e8779f4314f213c0861f4612b023f962072689
Danijel <dan@apix.org>
2023-08-10 00:14:33 +0200

    separate Ascending and Descending direction

--- apix/direction.py
+++ apix/direction.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, List, Tuple, TYPE_CHECKING
+from typing import Tuple, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
@@ -9,6 +9,8 @@ if TYPE_CHECKING:
 
 __all__ = [
     'ApixDirection',
+    'ApixAscendingDirection',
+    'ApixDescendingDirection',
 ]
 
 
@@ -16,19 +18,26 @@ class ApixDirection:
     name: str
     attribute: ApixAttribute
 
-    def __new__(cls, value: Any):
-        return super().__new__(cls)
-
-    def __init__(self, value: Any):
-        self.value = 1 if bool(value) else -1
+    def __init__(self, *args, **kwargs):
+        pass
 
     def __repr__(self) -> str:
-        return f'<{self.attribute.name}:direction:{self.value_string}>'
+        return f'<{self.attribute.name}:{self.name}>'
 
     @property
-    def value_string(self) -> str:
-        return 'asc' if self.value == 1 else 'desc'
+    def order(self) -> Tuple:
+        raise NotImplementedError
+
+
+class ApixAscendingDirection(ApixDirection):
 
     @property
-    def order(self) -> List[Tuple]:
-        return [(self.attribute.path_name, self.value)]
+    def order(self) -> Tuple:
+        return self.attribute.path_name, 1
+
+
+class ApixDescendingDirection(ApixDirection):
+
+    @property
+    def order(self) -> Tuple:
+        return self.attribute.path_name, -1
